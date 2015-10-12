52699f149ed53ccbffab4264836ab50dda8ae1b6
Hases Lee <hadesleeq@gmail.com>
2015-10-12 14:12:14 +0900

    package Ex

---
+++ src/tk/hadeslee/examples/MyBean.java
@@ -0,0 +1,20 @@
+package tk.hadeslee.examples;
+
+/**
+ * Project: HappyProgramming
+ * FileName: MyBean
+ * Date: 2015-10-12
+ * Time: 오후 2:09
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class MyBean {
+    protected int protectedInt;
+    public int pulicInt;
+    private int privateInt;
+    int defaultInt;
+
+    public MyBean() {
+    }
+}
