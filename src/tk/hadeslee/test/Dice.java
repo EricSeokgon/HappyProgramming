be5f92c2e8a547e41feba443948c7090238c0591
Hases Lee <hadesleeq@gmail.com>
2015-10-13 14:57:04 +0900

    Dice 인터페이스 구현

---
+++ src/tk/hadeslee/test/Dice.java
@@ -0,0 +1,19 @@
+package tk.hadeslee.test;
+
+/**
+ * Project: HappyProgramming
+ * FileName: Dice
+ * Date: 2015-10-13
+ * Time: 오후 2:51
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+
+//Dice라는 인터페이스가 정의되어있다.
+//Dice, 주사위인데, 어떤기능?
+public interface Dice {
+    //Dice는 점수를 반환하는 메소드를 선언하고 있다.
+    public int get();
+
+}
