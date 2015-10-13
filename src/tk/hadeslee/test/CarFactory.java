a9a8da8e101fbed8f8e603f78557c497f5298675
Hases Lee <hadesleeq@gmail.com>
2015-10-13 11:29:52 +0900

    자동차팩토리 클래스 상속

---
+++ src/tk/hadeslee/test/CarFactory.java
@@ -0,0 +1,29 @@
+package tk.hadeslee.test;
+
+import tk.hadeslee.examples.AbstractFactory;
+
+/**
+ * Project: HappyProgramming
+ * FileName: CarFactory
+ * Date: 2015-10-13
+ * Time: 오전 11:28
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class CarFactory extends AbstractFactory {
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void make() {
+
+    }
+
+    @Override
+    protected void clear() {
+
+    }
+}
