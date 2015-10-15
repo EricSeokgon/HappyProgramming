d8555e4b2e1acd68266d6d05af69e2d9839a55fd
Hases Lee <hadesleeq@gmail.com>
2015-10-15 14:42:45 +0900

    2차원 배열 선언과 동시에 초기화

--- src/tk/hadeslee/StringArrayTest.java
+++ src/tk/hadeslee/StringArrayTest.java
@@ -1,7 +1,5 @@
 package tk.hadeslee;
 
-import tk.hadeslee.examples.Car;
-
 /**
  * Project: HappyProgramming
  * FileName: StringArrayTest
@@ -18,7 +16,7 @@ public class StringArrayTest {
         stringArray[0] = "Hello";
         System.out.println(stringArray[0]);*/
 
-        int[] array = new int[]{5, 10, 15};
+        /*int[] array = new int[]{5, 10, 15};
         String[] array2 = new String[]{"hello", "world", "!!!"};
         Car[] array3 = new Car[]{new Car(), new Car(), new Car()};
         System.out.println(array.length);
@@ -32,7 +30,32 @@ public class StringArrayTest {
         System.out.println("---------------------------------------");
         for (int value : array) { //arry배열에서 값을 하나씩 꺼내서 value에게 저장.
             System.out.println(value);
+        }*/
+
+        int[][] array = new int[3][2];
+        //array변수가 참조하는 배열의 길이
+        System.out.println(array.length);
+        //array[0]변수가 참조하는 배열의 길이
+        System.out.println(array[0].length);
+        System.out.println("---------------------------------------");
+        int[][] array2 = new int[3][];
+        array2[0] = new int[2];
+        array2[1] = new int[3];
+        array2[2] = new int[4];
+        System.out.println(array2.length);
+        System.out.println(array2[0].length);
+        System.out.println(array2[1].length);
+        System.out.println(array2[2].length);
+        System.out.println("---------------------------------------");
+        int[][] array3 = new int[][]{{1, 2}, {1, 2, 3}, {1, 2, 3, 4}};
+        for (int i = 0; i < array3.length; i++) {//3번반복 :0,1,2
+            for (int k = 0; k < array3[i].length; k++) {
+                System.out.print(array3[i][k] + "\t");
+            }
+            System.out.println();
+
         }
+
 
     }
 }
