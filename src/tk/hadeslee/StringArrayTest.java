51000afc72286a17cd12ad274c315e4a7a5d65ec
Hases Lee <hadesleeq@gmail.com>
2015-10-15 13:59:32 +0900

    3개짜리 String을 참조하는 배열

---
+++ src/tk/hadeslee/StringArrayTest.java
@@ -0,0 +1,19 @@
+package tk.hadeslee;
+
+/**
+ * Project: HappyProgramming
+ * FileName: StringArrayTest
+ * Date: 2015-10-15
+ * Time: 오후 1:58
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class StringArrayTest {
+    public static void main(String[] args) {
+        //3개짜리 String을 참조하는 배열
+        String[] stringArray = new String[3];
+        stringArray[0] = "Hello";
+        System.out.println(stringArray[0]);
+    }
+}
