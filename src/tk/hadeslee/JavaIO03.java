ab3a67adf57c87a35522e7129204eb3df073fb9b
hadesyi <hadesleeq@gmail.com>
2015-11-03 14:26:49 +0900

    buffer의 0번째부터 읽어들인 수 만큼만 써라.

---
+++ src/tk/hadeslee/JavaIO03.java
@@ -0,0 +1,38 @@
+package tk.hadeslee;
+
+import java.io.*;
+
+/**
+ * Project: HappyProgramming
+ * FileName: JavaIO03
+ * Date: 2015-11-03
+ * Time: 오후 2:04
+ * Author: Hades Lee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+public class JavaIO03 {
+    public static void main(String[] args) throws Exception {
+        //InputStream, OutputStream은 추상 class.
+        InputStream in = null;
+        OutputStream out = null;
+
+        try {
+            //byte[]로 읽어들여서 파일에 저장하고 싳다.
+            in = new FileInputStream("./src/tk/hadeslee/JavaIO03.java");
+            out = new FileOutputStream("file.txt");
+
+            int readCount = 0; //읽어들인 수
+            byte[] buffer = new byte[1024];
+            while ((readCount = in.read(buffer)) != -1) {
+
+                out.write(buffer, 0, readCount);//buffer의 0번째부터 읽어 들인 수 만큼만 써라.
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            out.close();
+            in.close();
+        }
+    }
+}
